1cc4ac8c531a0cda42ac085223ff4021f8d9683f
evandougherty0 <doughertyevan7@gmail.com>
2023-10-24 12:28:21 -0400

    Change file struct -> coherncenetwork, + scorefunc

--- coherence_net.c
+++ coherence_net.c
@@ -1,13 +1,20 @@
 
-// struct for each individual file
-typedef struct file {
-    Node *nodesList;
-} File;
-
 // node for each idea in file
-typedef struct node {
+typedef struct idea_node {
     char *concept;
-    Node *connectNodes;
+    Node *connect_nodes;
 } Node;
 
+// struct for potential coherence network
+typedef struct coherence_network{
+    Node central_node;
+} Network;
+
 // Coherence Network Algorithm
+
+
+// Coherence Network Score
+int network_score(Network network) {
+    int score = 0;
+    return score;
+}
