79d54769491e3978ec88926604e71e1b142fa6c4
evandougherty0 <doughertyevan7@gmail.com>
2023-10-26 12:20:57 -0400

    Intial dummy socring function

--- coherence_net.c
+++ coherence_net.c
@@ -8,12 +8,13 @@ typedef struct idea_node {
 // struct for potential coherence network
 typedef struct coherence_network{
     Node central_node;
+    int* relatedFiles; // will need to number files to keep track of them
 } Network;
 
-// Coherence Network Algorithm
+// Coherence Network Algorithm: takes list of tuples and builds best coherence network based off of that
 
 
-// Coherence Network Score
+// Coherence Network Scoring function: takes Network network and returns the score
 int network_score(Network network) {
     int score = 0;
     return score;
