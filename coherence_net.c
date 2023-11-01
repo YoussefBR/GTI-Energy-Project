f2de476560e194ce97fd573eb37c8adca94b4663
evandougherty0 <doughertyevan7@gmail.com>
2023-11-01 17:21:32 -0400

    updarte to best_coherence alg outline

--- coherence_net.c
+++ coherence_net.c
@@ -24,10 +24,10 @@ Network best_coherence(Tuple file_tuples){
     int best_score = 0; // score of best coherence nework
     Network best_network; // best coherence network, instantiated to central_node.concept = NULL in next line
     best_network.central_node.concept = NULL;
-    // for each possible coherence network (brute force version)
-        // find the score
+    // for each possible coherence network (brute force version)  WE NEED TO FIND A WAY TO FIND EACH POSSIBLE COHERENCE NETWORK
+        // find the score: int current score = network_score(current_network, files_tuples)
         // if score better than best score: if(current_score > best_score) {
-            // make this network best newtork and score best score
+            // make this network best newtork and score best score: best_score = current_score; best_network = current_work; }
 }
 
 // Coherence Network Scoring function: takes Network network and returns the score
