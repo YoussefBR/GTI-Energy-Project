c75f7552dcd9269cfd6730cbd0d404cd019b230a
evandougherty0 <doughertyevan7@gmail.com>
2023-11-08 22:36:29 -0500

    EDITS to best_coherence function

--- coherence_net.c
+++ coherence_net.c
@@ -21,18 +21,22 @@ typedef struct tuple{
 } Tuple;
 
 // determines if two nodes are positively or negatively related
-bool isPositive(char *verb) {
-    return TRUE;
+bool isPositive(char verb[]) {
+    return true;
 }
 
 // Coherence Network Algorithm: takes list of tuples and builds best coherence network based off of that
-Network best_coherence(Tuple *file_tuples){
+Network best_coherence(Tuple file_tuples[]){
     int best_score = 0; // score of best coherence nework
     Network best_network; // best coherence network, instantiated to central_node.concept = NULL in next line
     best_network.central_node.concept = NULL;
+    Tuple *postive_tuples;
+    Tuple *negative_tuples;
     // for each possible coherence network (brute force version)  WE NEED TO FIND A WAY TO FIND EACH POSSIBLE COHERENCE NETWORK
-    for(int i = 0; i < sizeof(*file_tuples); i++) {
-
+    for(int i = 0; i < sizeof(file_tuples); i++) {
+        if(isPositve(file_tuples[i].verb)) {
+            
+        }
     }
         // find the score: int current score = network_score(current_network, files_tuples)
         // if score better than best score: if(current_score > best_score) {
