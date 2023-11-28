7dc8622f750424654b07736c8d880142b3f71cf6
evandougherty0 <doughertyevan7@gmail.com>
2023-11-28 13:02:40 -0500

    co_net_practice.c

--- co_net_practice.c
+++ co_net_practice.c
@@ -2,5 +2,12 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include "idea_node.h"
+#include "coherence_net.h"
 
 char *concepts[] = {"pizza", "noodles", "pasta", "sauce", "tacos", "cheese", "beef", "lettuce", "beans", "chicken", "bacon", "stocks", "money", "trash", "a", "b", "cat", "dog", "pig", "bug", "ant", "bee", "wasp"};
+
+int main(void) {
+    Network network = build_network(concepts, 23);
+    printf(network.central_node.concept);
+    return 0;
+}
