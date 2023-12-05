18532c3d051be96ef0aa05521cc4eacc334cc117
evandougherty0 <doughertyevan7@gmail.com>
2023-12-05 13:31:49 -0500

    cleanup and added comments through line 132

--- coherence_network.py
+++ coherence_network.py
@@ -1,104 +1,70 @@
 import networkx as nx
 import random
-#import math
 
+# object that basically extends networkx graph by adding a tracker for which node is the center
 class coherence_network:
     def __init__(self, graph, center):
         self.graph = graph
         self.center = center
 
-# adds next node and recursively calls at 50% chance, then out at 50% chance
+# adds random number of edges to the node "concept"
 def next_connection(triples, num_triples, td, prev_concept, concept, weight, G):
-    
+     # sets node depth at 3
     if weight < .3:
         return
-    
+    # randomly decide how many and which of the connections to add
     num_to_add = random.randint(1, len(td[concept]))
     order = list(range(len(td[concept])))
     random.shuffle(order)
-
-    #next_concept = concepts[order[count[0]]]
-    #G.add_node(next_concept)
-    
+    # add chosen edges to network
     for x in range(num_to_add):
-
         if not td[concept][order[x]] == prev_concept:
-
             G.add_edge(concept, td[concept][order[x]], weight = weight)
-
-    #count[0] += 1
-
+            # 50% chance to add further connections to each node that is being added
             if random.random() > 0.5:
                 next_connection(triples, num_triples, td, concept, td[concept][order[x]], weight * 0.5, G)
 
-    # 33% chance of continuing horizontally off the previous node
-    #while count[0] < num_concepts and random.random() < 0.33:
-    #    next_connection(concepts, num_concepts, count, order, prev_concept, weight, G)
-
-# builds random coherence network from given concepts
-def build_network(triples, triple_dict):
-    
+# builds random coherence network from given triples
+def build_network(triples, triple_dict): 
     G = nx.Graph()
     num_triples = len(triples)
-
-    # Randomly mix up the order of concepts added to the network
-    #top_20_percent = math.ceil((num_triples * .2))
-    #order = list(range(top_20_percent))
-    #random.shuffle(order)
-
-    # Add center
+    # Add center, randomly chosen from top 20% highest scoring triples, 50/50 chance of choosing the subject or object
     if random.random() > .5:
         center_concept = triples[(random.randint(0,int(num_triples * .2)))].subject
     else:
         center_concept = triples[(random.randint(0,int(num_triples * .2)))].object
     G.add_node(center_concept)
     co_net = coherence_network(G, center_concept)
-
-    #order = list(range(num_triples))
-
-    # Tracking
-    #count = [1]  # to track count of concepts, as a list to update cross-function
-    #cont = 2  # Decides whether to continue or not at 80% chance, initialized to continue
-
+    # randomly decide how many and which of the connections to add to the center node
     num_to_add = random.randint(1, len(triple_dict[center_concept]))
     order = list(range(len(triple_dict[center_concept])))
     random.shuffle(order)
-
+    # add chosen edges
     for x in range(num_to_add):
-        
         G.add_edge(center_concept, triple_dict[center_concept][order[x]], weight = 1)
-
-        #next_concept = concepts[order[count[0]]]
-        #G.add_node(next_concept)
-        #G.add_edge(center_concept, next_concept, weight=1)
-
-        #count[0] += 1
-
-        # 67% chance to add a random node onto the last node
+        # 67% chance to connections to each node, edge weights decrease by half for each increase depth
         if random.random() > 0.33:
             next_connection(triples, num_triples, triple_dict, center_concept, triple_dict[center_concept][order[x]], 0.5, G)
-
-        # 80% chance to continue
-        #cont = random.randint(0, 4)
-
     return co_net
 
-# scores coherence of network 
+# scores coherence of network based off distance from center and their frequency value, as well as prunes unconnected nodes
 def netwowk_score(graph, frequencies):
     score = 0
-    # assuming frequencies is dictionary such that {(concept1, concept2) : frequency_of_ideas_in_triples}, score becomes sum of freuencies with wieght based on how far from center
     nodes_to_remove = []
     for edge in graph.edges:
         node1, node2 = edge
+        # finds unconnected nodes to remove for pruning
         if graph[node1][node2]['weight'] == 0:
             if node1 not in nodes_to_remove:
                 nodes_to_remove.append(node1)
             if node2 not in nodes_to_remove:
                 nodes_to_remove.append(node2)
+        # scores each node
         if (node1, node2) in frequencies:
             score += graph[node1][node2]['weight'] * frequencies[(node1, node2)]
         if (node2, node1) in frequencies:
             score += graph[node1][node2]['weight'] * frequencies[(node2, node1)]
+    # prunes unconected nodes
     graph.remove_nodes_from(nodes_to_remove)
     return score
 
@@ -109,12 +75,13 @@ def add_t_edge(G, nodes):
     G.add_node(node2)
     G.add_edge(node1, node2, weight = 0)
 
-# helper function that recursively calls for give_weights
+# helper function that recursively calls for give_weights, and prunes nodes of depth > 3
 def rec_weights(G, node, prev_node, weight):
     nodes_to_remove = []
     for edge in G.edges(node):
         n1, n2 = edge
         if not (n1 == prev_node or n2 == prev_node):
+            # check if this is the shortest path to this edge before updating weight
             if G[n1][n2]['weight'] < weight:
                 G[n1][n2]['weight'] = weight
                 # find weights for next node
@@ -122,27 +89,30 @@ def rec_weights(G, node, prev_node, weight):
                     nodes_to_remove.extend(rec_weights(G, n2, node, weight * .5))
                 else:
                     nodes_to_remove.extend(rec_weights(G, n1, node, weight * .5))
+    # finds nodes that have depth > 3 for pruning
     if weight == .25:
         nodes_to_remove.append(node)
     return nodes_to_remove
 
-# give weights to edges based on location in network
+# give weights to edges in network based on depth (starting from center node)
 def give_weights(network):
     nodes_to_remove = []
     for edge in network.graph.edges(network.center):
         n1, n2 = edge
+        # sets weight of depth 1 edges to 1
         network.graph[n1][n2]['weight'] = 1
-        # find weights for connceted nodes
+        # sets the weights for edges that are of depth 2 and so on, decreasing weight by half as depth incereases by 1
         if n1 == network.center:
             nodes_to_remove = rec_weights(network.graph, n2, network.center, .5)
         else:
             nodes_to_remove = rec_weights(network.graph, n1, network.center, .5)
+    # prune nodes of depth > 3
     for node in nodes_to_remove:
         if network.graph.has_node(node):
             network.graph.remove_node(node)
     return
 
-# does crossover of networks
+# does crossover of 2 networks
 def crossover(parent1, parent2):
     G1 = nx.Graph()
     G2 = nx.Graph()
@@ -152,14 +122,14 @@ def crossover(parent1, parent2):
         edges1.append(edge)
     for edge in parent2.graph.edges():
         edges2.append(edge)
-    # add first center edge to each to ensure center is carried correctly
+    # add first center edge to each
     len1 = len(edges1)
     len2 = len(edges2)
     if len1 > 0:
         add_t_edge(G1, edges1[0])
     if len2 > 0:
         add_t_edge(G2, edges2[0])
-    # add edges 80% p1->c1 and p2->c2 for each edge and 20% chance p1->c2 and p2->c1
+    # add edges to child graphs, for each edge, pick an edge from each parent, 80% chance p1->c1 and p2->c2 for each edge and 20% chance p1->c2 and p2->c1
     if len1 > len2:
         for x in range(1, len2):
             if random.random() > .2:
@@ -196,19 +166,11 @@ def crossover(parent1, parent2):
 
 #finds the best coherence network by randomlmy generating networks based off the triples, scoring them on their coherence and optimzing the best one with the genetic algorithm
 def find_best_network(triples, frequencies, trip_dict):
-    # make a list of concepts
-    #concepts = []
-    #for triple in triples:
-    #    if triple.subject not in concepts:
-    #        concepts.append(triple.subject)
-    #    if triple.object not in concepts:
-    #        concepts.append(triple.object)
-    # find best coherence network
     best_network = None
     best_score = 0
     first = True
     networks = [] # tracks networks and score
-    for x in range(100):
+    for x in range(1000):
         # fills out preiouvsly held 40 networks
         if first:
             for y in range(80):
