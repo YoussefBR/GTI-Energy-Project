7c4d224aec84f6533466dc7589c3a0c23c438d0b
evandougherty0 <doughertyevan7@gmail.com>
2023-12-05 13:53:08 -0500

    better name for extraction_evan

---
+++ extract_&_find_network.py
@@ -0,0 +1,34 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+import coherence_network as cn
+import triple_extraction as te
+
+class triple:
+     def __init__(self, subject, verb, object, frequency):
+        self.subject = subject
+        self.verb = verb
+        self.object = object
+        self.frequency = frequency
+
+te.main()
+extracted_triples = te.getTriples()
+connections = te.getWordConnections()
+
+# convert triples from file into triples the coherence network can handle
+triples = []
+frequencies = {}
+for e_triple in extracted_triples:
+    triples.append(triple(e_triple[0][0], e_triple[0][1], e_triple[0][2], e_triple[1]))
+    frequencies[(e_triple[0][0], e_triple[0][2])] = e_triple[1]
+
+# find best network
+co_net, score = cn.find_best_network(triples, frequencies, connections)
+
+# output center
+print("center:", {co_net.center}, "score:", score)
+
+# Draw the graph
+nx.draw(co_net.graph, with_labels=True, node_color='skyblue', node_size=700, font_size=20)
+
+# Show the plot
+plt.show()
